510f4e0a590079f86ae6ff0d21b99c3d3e0a70a4
Luangoma <101829176+Luangoma@users.noreply.github.com>
2025-06-13 21:24:17 +0200

    Correcion pantalla con slider

--- EMZERO/Assets/Scripts/GameManager.cs
+++ EMZERO/Assets/Scripts/GameManager.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : NetworkBehaviour
 {
     // Start is called before the first frame update
     void Start()
